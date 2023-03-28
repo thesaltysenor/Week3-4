bdfb415a2f9bea15f7de7a1210fa0f7f9ea77747
thesaltysenor <126297489+thesaltysenor@users.noreply.github.com>
2023-03-27 21:38:38 -0500

    third Commit

--- src/Week3Assignment/Assignment.java
+++ src/Week3Assignment/Assignment.java
@@ -17,12 +17,32 @@ public class Assignment {
 
       String[] names = {"Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"};
         int sumOfLetters = 0;
-        for (String name : names) {
-            sumOfLetters += name.length();
+         for (int i = 0; i < names.length; i++) {
+             sumOfLetters += names[i].length();
+         }
 
-            System.out.println(sumOfLetters);
+         double averageLettersPerName = sumOfLetters / names.length;
+         System.out.println(averageLettersPerName);
+
+         String allNames = "";
+
+         for (int i = 0; i < names.length; i++) {
+            allNames += names[i];
+            if(i < names.length - 1) {
+                allNames += " ";
+            }
+         }
+
+         System.out.println(allNames);
+
+         // 3. you use a .length - 1, that way it will not stop at the end of the array and keep
+        // it dynamic if you need to add more to the array
+        // 4. you would use [0] since 0 marks the first element
+
+        int[] nameLengths;
+
 
             // I Hope this makes it to github please
-        }
+
     }
 }
